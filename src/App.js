400e2a91c8706c40f6466fba19ab5aed5f8342f4
ali sobirov <muhammadali@limefriends.com>
2023-08-21 22:27:28 +0900

    project setup

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import "./App.css";
 import SuperHeroes from "./components/SuperHeroes.page";
 import RQSuperHeroes from "./components/RQSuperHeroes.page";
 import Home from "./components/Home.page";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/super-heroes' element={<SuperHeroes />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroes />} />
 
-          <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
